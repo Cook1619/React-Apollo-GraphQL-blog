504bdb98d0f307b0a2e92dd9ec8cc79d6f1cd3f4
Cook1619 <cook6348@gmail.com>
2020-08-18 13:59:55 -0500

    Starting mern blog fullstack app

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("This is my express server!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
