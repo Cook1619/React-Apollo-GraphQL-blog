529eed4b68e48c07b30f4439b79f45cf8cf4abe3
Cook1619 <cook6348@gmail.com>
2020-08-19 15:12:38 -0500

    Setting up basic apolle server, graphql schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,12 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { blogPosts } from "./blogpost";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
+
 const app = express();
 const PORT = 8000;
 
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.send("This is my express server!!!");
-});
-
-app.get("/blogposts", (req, res) => {
-  res.send(blogPosts);
-});
-
-app.post("/delete-post", (req, res) => {
-  const id: string = req.body.id;
-  for (let i = 0; i < blogPosts.length; i++) {
-    if (blogPosts[i].id === id) {
-      return res.send(blogPosts.splice(i, 1)[0]);
-    }
-  }
-  res.send("failed to delete blog post");
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
